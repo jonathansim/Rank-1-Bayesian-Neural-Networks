dc1d702bb714ae173ae806012deccbb1b0050f77
jonathansim <93523007+jonathansim@users.noreply.github.com>
2024-05-07 18:09:09 +0200

    Updated results processing script

--- results_processing.py
+++ results_processing.py
@@ -19,11 +19,34 @@ accuracies = [d['accuracy'] for d in results]
 nll = [d['average_nll'] for d in results]
 ece = [d['ece'] for d in results]
 
-# Create a plot of accuracy over epochs 
-plt.figure(figsize=(10, 5))
+# Create a plots of metrics over epochs 
+plt.figure(figsize=(20, 7))
+
+
+# Subplot for Accuracy
+plt.subplot(1, 3, 1)  # 1 row, 3 columns, 1st subplot
 plt.plot(epochs, accuracies, marker='o', linestyle='-', color='b')
-plt.title('Model Accuracy Over Epochs')
+plt.title('Model Validation Accuracy Over Epochs')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.grid(True)
+
+# Subplot for NLL
+plt.subplot(1, 3, 2)  # 1 row, 3 columns, 2nd subplot
+plt.plot(epochs, nll, marker='o', linestyle='-', color='r')
+plt.title('Validation Negative Log Likelihood Over Epochs')
+plt.xlabel('Epoch')
+plt.ylabel('NLL')
+plt.grid(True)
+
+# Subplot for ECE
+plt.subplot(1, 3, 3)  # 1 row, 3 columns, 3rd subplot
+plt.plot(epochs, ece, marker='o', linestyle='-', color='g')
+plt.title('Validation Expected Calibration Error Over Epochs')
+plt.xlabel('Epoch')
+plt.ylabel('ECE')
+plt.grid(True)
+
+# Show plot
+plt.tight_layout()
 plt.show()
