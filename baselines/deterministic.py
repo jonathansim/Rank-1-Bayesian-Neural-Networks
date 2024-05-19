487dd7cb9195105ce4085867cecd2463cd1f4e6f
jonathansim <93523007+jonathansim@users.noreply.github.com>
2024-05-19 16:50:33 +0200

    updated deterministic script

--- baselines/deterministic.py
+++ baselines/deterministic.py
@@ -20,7 +20,7 @@ from data_utils import load_data
 # Add parsing functionality 
 parser = argparse.ArgumentParser(description='Deterministic Wide ResNet (on CIFAR 10)')
 
-parser.add_argument('--epochs', type=int, default=20, help='number of epochs to train')
+parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train')
 parser.add_argument('--batch-size', type=int, default=128, help='input mini-batch size for training')
 parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
 parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
@@ -28,7 +28,7 @@ parser.add_argument('--nesterov', default=True, type=bool, help='nesterov moment
 parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float, help='weight decay')
 parser.add_argument('--seed', default=1, type=int, help="seed for reproducibility")
 parser.add_argument('--use-scheduler', default=True, type=bool, help="Whether to use a scheduler for the LR or not")
-parser.add_argument('--subset-size', default=1000, type=int, help="number of data samples used (if None, all used) (for debugging locally)")
+parser.add_argument('--use-subset', default=True, type=bool, help="whether to use a subset (for debugging locally) or all data")
 
 
 def set_training_seed(seed):
@@ -42,6 +42,12 @@ def train(model, device, train_loader, optimizer, criterion, epoch, scheduler=No
     print('\nEpoch: %d' % epoch)
     model.train()
     running_loss = 0
+    epoch_loss = 0 # to store total loss for the epoch
+    num_batches = len(train_loader)
+    correct = 0
+    total = 0
+    results = {}
+
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -55,15 +61,36 @@ def train(model, device, train_loader, optimizer, criterion, epoch, scheduler=No
             scheduler.step()
 
         running_loss += loss.item()
+        epoch_loss += loss.item()
+
+        _, predicted = output.max(1)
+        total += target.size(0)
+        correct += predicted.eq(target).sum().item()
+
         if batch_idx % 100 == 99:    # print every 100 mini-batches
             print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, batch_idx + 1, running_loss / 100)) # print epoch, batch_index and average loss over that mini-batch
+                  (epoch, batch_idx + 1, running_loss / 100)) # print epoch, batch_index and average loss over that mini-batch
             running_loss = 0.0
+        
+    # Store the epoch loss and accuracy
+    average_epoch_loss = epoch_loss / num_batches
+    train_accuracy = 100 * correct / total
+    current_lr = optimizer.param_groups[0]['lr']
 
-def validate(model, test_loader, device, epoch, metrics=None):
+    results["epoch"] = epoch
+    results["avg_epoch_loss"] = average_epoch_loss
+    results["train_accuracy"] = train_accuracy
+    results["lr"] = current_lr
+
+    print(f"Epoch {epoch} Training Loss: {average_epoch_loss:.3f}, Training Accuracy: {train_accuracy:.2f}%")
+    return results 
+
+def evaluate(model, test_loader, device, epoch=None, metrics=None, phase="Validation"):
     model.eval()
     total = 0
     total_nll = 0.0
+    results = {}
+
     with torch.no_grad():
         for (inputs, labels) in test_loader: 
             inputs, labels = inputs.to(device), labels.to(device)
@@ -73,18 +100,18 @@ def validate(model, test_loader, device, epoch, metrics=None):
             log_probs = F.log_softmax(outputs, dim=1) 
             nll_loss = F.nll_loss(log_probs, labels)
             total_nll += nll_loss.item() * inputs.size(0)
-            # print(f"the total NLL is {total_nll}")
-            
             total += labels.size(0)
-            
-            
+               
             if metrics:
                 for metric in metrics.values():
                     metric.update(outputs, labels)
 
+    if epoch is not None:
+        results["epoch"] = epoch
+
     # Calculate final metric values
-    results = {"epoch": epoch,
-               "average_nll": total_nll / total}
+    results["average_nll"] = total_nll / total
+
     
     # Compute and store results for any additional metrics
     if metrics:
@@ -97,18 +124,22 @@ def validate(model, test_loader, device, epoch, metrics=None):
     # print(f'Accuracy of this network is {results["accuracy"]}')
     # print(f'ECE of this network is {results["ece"]}')
     # print(f"The average NLL of this network is {results["average_nll"]}")
-    print(f"The validation results are as follows: {results}")
+    print(f"{phase} Results: {results}")
     return results
 
 
 def main():
     # Parse arguments
-    args = parser.parse_args(args=[])
+    args = parser.parse_args()
     training_seed = args.seed
     batch_size = args.batch_size
-    subset_size = args.subset_size 
+    if args.use_subset: 
+        subset_size = 1000
+    else: 
+        subset_size = None
     data_seed = 42 # seed used for data loading (e.g. transformations)
     
+    print(f"Total number of epochs {args.epochs}")
     # Set device
     if torch.cuda.is_available():
         device = torch.device("cuda")
@@ -143,20 +174,34 @@ def main():
         scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, len(train_loader)*args.epochs)
 
     # Results for each epoch
+    all_train_results = []
     all_val_results = []
     for epoch in range(args.epochs):
-        train(model=model, device=device, train_loader=train_loader, optimizer=optimizer, criterion=criterion, epoch=epoch, scheduler=scheduler)
-        val_results = validate(model=model, test_loader=val_loader, device=device, epoch=epoch, metrics=metrics)
+        train_results = train(model=model, device=device, train_loader=train_loader, optimizer=optimizer, criterion=criterion, epoch=epoch, scheduler=scheduler)
+        val_results = evaluate(model=model, test_loader=val_loader, device=device, epoch=epoch, metrics=metrics, phase="Validation")
+        all_train_results.append(train_results)
         all_val_results.append(val_results)
     
-    print(all_val_results)
+    # Whether to perform evaluation on the testing set
+    test_metrics = None
+    if args.use_subset is False: 
+        test_metrics = evaluate(model=model, test_loader=test_loader, device=device, metrics=metrics, phase="Testing")
 
     # Save results for later
     current_time = datetime.now().strftime("%m-%d-H%H")
-    filename = f'det_results_{current_time}.json'
+    filename_train = f'det_train_results_{current_time}.json'
+    filename_val = f'det_val_results_{current_time}.json'
+    filename_test = f'det_test_results_{current_time}.json'
 
-    with open(filename, 'w') as file:
+    with open(filename_train, 'w') as file:
+        json.dump(all_train_results, file)
+    
+    with open(filename_val, 'w') as file:
         json.dump(all_val_results, file)
+    
+    if test_metrics is not None: 
+        with open(filename_test, 'w') as file:
+            json.dump(test_metrics, file)
 
 if __name__ == '__main__':
    main()
