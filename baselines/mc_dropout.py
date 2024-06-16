262525491ef1aabe060d71fa41b5eb53582bd2a7
jonathansim <93523007+jonathansim@users.noreply.github.com>
2024-06-16 22:32:05 +0200

    changed flags

--- baselines/mc_dropout.py
+++ baselines/mc_dropout.py
@@ -31,14 +31,14 @@ parser.add_argument('--nesterov', default=True, type=bool, help='nesterov moment
 parser.add_argument('--weight-decay', '--wd', default=3e-4, type=float, help='weight decay')
 parser.add_argument('--seed', default=1, type=int, help="seed for reproducibility")
 parser.add_argument('--use-scheduler', default=True, type=bool, help="Whether to use a scheduler for the LR or not")
-parser.add_argument('--use-subset', default=True, type=bool, help="whether to use a subset (for debugging locally) or all data")
+parser.add_argument('--use-subset', default=False, type=bool, help="whether to use a subset (for debugging locally) or all data")
 parser.add_argument('--wandb', default="online", type=str, choices=["online", "disabled"] , help="whether to track with weights and biases or not")
 parser.add_argument('--warmup-epochs', default=1, type=int, help="Number of warmup epochs")
 parser.add_argument('--scheduler', default="warm", type=str, choices=["warm", "cosine", "multistep", "none"], help="which scheduler to use")
 
 # MC Dropout specific arguments
 parser.add_argument('--droprate', default=0.1, type=float, help="dropout probability")
-parser.add_argument('--forward-passes', default=1, type=int, help="number of MC dropout forward passes for validation")
+parser.add_argument('--forward-passes', default=3, type=int, help="number of MC dropout forward passes for validation")
 
 
 def set_training_seed(seed):
