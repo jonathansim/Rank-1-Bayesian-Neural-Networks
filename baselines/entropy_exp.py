47f0809e788d8d869164ee4ad59cdc032455e8c1
jonathansim <93523007+jonathansim@users.noreply.github.com>
2024-07-03 15:09:10 +0200

    add deterministic entropy exp

--- baselines/entropy_exp.py
+++ baselines/entropy_exp.py
@@ -14,19 +14,15 @@ from torchmetrics.classification import CalibrationError
 import random
 import wandb
 
-
-from rank1_wide_resnet import Rank1Bayesian_WideResNet
 from data_utils import load_data
 from corrupted_data_utils import load_corrupted_data
+from wide_resnet import WideResNet
 
 # Add parsing functionality 
-parser = argparse.ArgumentParser(description='Evaluate script for model (BNN)')
+parser = argparse.ArgumentParser(description='Evaluate script for model (Deterministic)')
 
 # General arguments
 parser.add_argument('--model', type=str, default="placeholder", help='Path of model')
-parser.add_argument('--num-eval-samples', default=10, type=int, help="Number of samples to use for evaluation")
-parser.add_argument('--ensemble-size', default=4, type=int, help="Number of models in the ensemble")
-
 
 # Function to compute entropy
 def compute_entropies(probabilities):
@@ -37,7 +33,7 @@ def compute_entropies(probabilities):
     entropies = - (probabilities * torch.log(probabilities)).sum(dim=1)
     return entropies
 
-def evaluate(model, device, test_loader, num_eval_samples, dataset="normal"):
+def evaluate(model, device, test_loader, dataset="normal"):
     seed = 42
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)  # If you are using CUDA
@@ -52,17 +48,11 @@ def evaluate(model, device, test_loader, num_eval_samples, dataset="normal"):
     with torch.no_grad():
         for (inputs, labels) in test_loader: 
             inputs, labels = inputs.to(device), labels.to(device)
+            logits = model(inputs)
             
-            # Handle multiple samples
-            logits = torch.stack([model(inputs) for _ in range(num_eval_samples)], dim=2) # Shape: (batch_size*ensemble_size, num_classes, num_eval_samples)
-            logits = logits.view(model.ensemble_size, -1, num_classes, num_eval_samples) # Shape: (ensemble_size, batch_size, num_classes, num_eval_samples)
-            logits = logits.permute(1, 2, 0, 3) # Shape: (batch_size, num_classes, ensemble_size, num_eval_samples)
             probs = torch.softmax(logits, dim=1)
 
-            # Average probs over ensemble_size and num_eval_samples, make predictions and compute accuracy
-            mean_probs = probs.mean(dim=(2, 3)) # Shape: (batch_size, num_classes)
-
-            batch_entropies = compute_entropies(mean_probs)
+            batch_entropies = compute_entropies(probs)
             all_entropies.append(batch_entropies.cpu().numpy())
 
     # Convert all entropies to a single numpy array
@@ -74,7 +64,7 @@ def main():
     args = parser.parse_args()
 
     # Wandb
-    run_name = "entropy_exp_BNN"
+    run_name = "entropy_exp_DET"
     wandb.init(project='evaluation_only', mode="online", name=run_name)
     wandb.config.update(args)
 
@@ -88,27 +78,25 @@ def main():
     print(f"Using device {device}")
 
     # Load model
-    model = Rank1Bayesian_WideResNet(depth=28, widen_factor=10, num_classes=10, ensemble_size=args.ensemble_size)
-    saved_model_bnn_path = args.model
-    model.load_state_dict(torch.load(saved_model_bnn_path, map_location=torch.device('cpu')))
+    model = WideResNet(depth=28, widen_factor=10, num_classes=10)
+    saved_model_det_path = args.model
+    model.load_state_dict(torch.load(saved_model_det_path, map_location=torch.device('cpu')))
     model.to(device)
 
     # Load data
     batch_size = 128
     corrupted_data_loader, normal_data_loader = load_corrupted_data(batch_size=batch_size, seed=5)
     
-    # Define the number of evaluation samples to test
-    num_eval_samples = args.num_eval_samples
 
     # Evaluate the model on the normal data
-    corrupted_entropies = evaluate(model, device, corrupted_data_loader, num_eval_samples, dataset="corrupted")
-    normal_entropies = evaluate(model, device, normal_data_loader, num_eval_samples, dataset="normal")
+    corrupted_entropies = evaluate(model, device, corrupted_data_loader, dataset="corrupted")
+    normal_entropies = evaluate(model, device, normal_data_loader, dataset="normal")
 
     # Save the lists as separate files within an artifact
-    artifact = wandb.Artifact('entropies_lists', type='dataset')
-    with artifact.new_file('BNN_normal_data_entropy.txt') as f:
+    artifact = wandb.Artifact('entropies_lists_det', type='dataset')
+    with artifact.new_file('det_normal_data_entropy.txt') as f:
         f.write('\n'.join(map(str, normal_entropies)))
-    with artifact.new_file('BNN_corrupt_data_entropy.txt') as f:
+    with artifact.new_file('det_corrupt_data_entropy.txt') as f:
         f.write('\n'.join(map(str, corrupted_entropies)))
 
     # Log the artifact
